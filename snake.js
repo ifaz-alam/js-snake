e7a6db42dcc8852283dfa6212ce33ae0449122c3
Ifaz Alam <ifazalam33@gmail.com>
2024-02-19 13:22:29 -0500

    Initial Setup

---
+++ snake.js
@@ -0,0 +1,26 @@
+// Constants for board dimensions and block size
+var _a = [20, 10], rows = _a[0], cols = _a[1];
+var blockSize = 25;
+// Variables for canvas and its context
+var board;
+var context;
+// Snake Information
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+// Function to update canvas content
+var update = function () {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+    context.fillStyle = "lime";
+    context === null || context === void 0 ? void 0 : context.fillRect(snakeX, snakeY, blockSize, blockSize);
+};
+// Window onload event handler
+window.onload = function () {
+    board = document.getElementById("board");
+    if (board) {
+        board.height = rows * blockSize;
+        board.width = cols * blockSize;
+        context = board.getContext("2d");
+        update();
+    }
+};
---
+++ snake.ts
@@ -0,0 +1,34 @@
+// Constants for board dimensions and block size
+const [rows, cols]: [number, number] = [20, 10];
+const blockSize: number = 25;
+
+// Variables for canvas and its context
+let board: HTMLCanvasElement | null;
+let context: CanvasRenderingContext2D | null;
+
+// Snake Information
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+// Function to update canvas content
+const update = () => {
+  context!.fillStyle = "black";
+  context!.fillRect(0, 0, board!.width, board!.height);
+
+  context!.fillStyle = "lime";
+  context?.fillRect(snakeX, snakeY, blockSize, blockSize);
+};
+
+// Window onload event handler
+window.onload = () => {
+  board = document.getElementById("board") as HTMLCanvasElement | null;
+
+  if (board) {
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+
+    context = board.getContext("2d");
+
+    update();
+  }
+};
